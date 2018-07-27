f9827a471d2d3110f790d8e3a9fd7ef327873fbb
rumis <rumis@live.ru>
2018-07-27 21:24:53 +0300

    init

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+svg4everybody(); // иницализация полифила для IE
+
+$(document).ready(function(){
+  // весь ваш код c jQuery
+
+  AOS.init();
+});
+
+// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
+// function ready(fn) {
+//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// ready(function(){
+//   // code
+// });
